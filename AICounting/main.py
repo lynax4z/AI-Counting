bf957287132feb8b98782a86062c9d14c1835dc7
Vencent Hortaleza Pepito <65444767+lynax4z@users.noreply.github.com>
2025-12-08 09:08:34 +0700

    Update main.py
    updates for fps information

--- AICounting/main.py
+++ AICounting/main.py
@@ -15,14 +15,28 @@ if not cap.isOpened():
     print('camera is not opened')
     exit()
 
+prev_tick = cv2.getTickCount()
+freq = cv2.getTickFrequency()
+fps_smooth = 0.0
+
 while True:
     ret, frame = cap.read()
     if not ret:
         print("can't receive the frame")
         break
+    current_tick = cv2.getTickCount()
+    dt = (current_tick - prev_tick) / freq  # detik
+    prev_tick = current_tick
+    if dt > 0:
+        fps_inst = 1.0 / dt   # FPS instan
+        # smoothing: 90% nilai lama, 10% nilai baru
+        fps_smooth = fps_inst if fps_smooth == 0 else (0.9 * fps_smooth + 0.1 * fps_inst)
+    else:
+        fps_inst = 0.0
     boxes = optimize.detect(frame)
     count = len(boxes)
     cv2.putText(frame, f'count : {count}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.putText(frame, f'FPS : {fps_smooth:.2f}', (10, 110), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.imshow('test', frame)
     if cv2.waitKey(1)&0xFF == ord('q'):
         break
